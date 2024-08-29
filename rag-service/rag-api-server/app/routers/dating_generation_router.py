e5c1d3977841676fd11d3bcc8fff41d13a99bc0b
jwywoo <jwywoo26@gmail.com>
2024-08-29 10:59:41 +0900

    [FEAT] #10-3 RAG API: Schema for request and response set
    - request_dto/request_dating_request_dto.py
    - response_dto/response_dating_response_dto.py

--- rag-service/rag-api-server/app/routers/dating_generation_router.py
+++ rag-service/rag-api-server/app/routers/dating_generation_router.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
+from ..schema.request_dto.dating_generation_request_dto import DatingGenRequestDto
+from ..schema.response_dto.dating_generation_response_dto import DatingGenResponseDto
+
 router = APIRouter()
 
 @router.post("/dating/generate")
-def create_dating_generation():
+def create_dating_generation(request: DatingGenRequestDto):
     return {"message":"Dating!"}
