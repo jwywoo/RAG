868d1a17f9bd993db3fba5cbb8cedf312426406e
jwywoo <jwywoo26@gmail.com>
2024-09-03 11:55:30 +0900

    [FEAT] #24-3 Stable Server: Global Exception Handler
    - 500: Message -> Please try again later

--- rag-service/rag-api-server/app/main.py
+++ rag-service/rag-api-server/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
 from dotenv import load_dotenv
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from .routers import dating_generation_router
 
@@ -11,6 +13,16 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Server Init
 app = FastAPI()
+
+# Exception
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"message": "Please try again later."},
+    )
 
 # Router
 app.include_router(dating_generation_router.router)
